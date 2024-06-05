e893af49775a42b25d1ed43c88d02b342a82b511
Maxim <mbazhukov@gmail.com>
2024-06-05 20:27:48 +0300

    my first c file test

--- testtask/first.c
+++ testtask/first.c
@@ -1,3 +1,20 @@
-//
-// Created by Maxim Bazhukov on 05/06/2024.
-//
+#include <stdio.h>
+
+void mirror(int *px, int *py)
+{
+    *px = *px * (-1);
+    *py = *py;
+
+}
+
+
+int main(){
+    int x, y;
+    scanf("%d%d", &x ,&y);
+
+    mirror(&x, &y);
+
+    printf("%d %d\n", x, y);
+
+    return 0;
+}
